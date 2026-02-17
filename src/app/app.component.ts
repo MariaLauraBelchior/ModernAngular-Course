3f29a6550f0236e03896143560394c031795d7a4
MariaLauraBelchior <marialaurabelchior007@gmail.com>
2026-02-17 19:26:11 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HelloComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'modern-angular';
+}
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.component.html',
+  styleUrl: './hello.component.scss'
+})
+export class HelloComponent {
+
+  protected title = 'Modern Angular';
+
+  protected isDisabled = false;
+
+  protected onClick(): void {
+    console.log('Button clicked!');
+    this.isDisabled = !this.isDisabled;
+  }
+}
